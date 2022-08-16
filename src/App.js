e7cf31262b4827f89f748f76fcc01332b93d6d3a
ryyy20 <36107726+ryyy20@users.noreply.github.com>
2022-08-16 08:05:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,19 +13,7 @@ class App extends Component {
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
               <img src={GreyJedi} className="App-GreyJedi" alt="GreyJedi" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
